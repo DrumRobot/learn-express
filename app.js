b9aee1ce2ae1e148652814de54b5a8951f47c421
David Jeong <drumrobot43@gmail.com>
2023-03-20 16:09:10 +0900

    t

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 dotenv.config();
 const app = express();
+const secret = process.env.PASSWORD || process.env.COOKIE_SECRET;
 const port = process.env.PORT || 8888;
 
 app.use(morgan('dev'));
@@ -25,15 +26,35 @@ app.use(session({
   name: 'session-cookie',
 }));
 
+/* 로그인 미들웨어 */
+app.post('/login', (req, res) => {
+  let { username, password } = req.body;
+  /* 로그인 성공시 */
+  if (password === secret) {
+    req.session.user = { username };
+    res.redirect('/');
+  } else {
+    // TODO: 로그인 실패 처리
+    res.redirect('/login.html');
+  }
+});
 /* 전처리 미들웨어 */
 app.use((req, res, next) => {
   console.log('모든 요청에 다 실행됩니다.');
-  next();
+  if (req.session.user === undefined) {
+    res.redirect('login.html');
+  } else {
+    next();
+  }
 });
 /* 라우트 미들웨어 */
 app.get('/', (req, res, next) => {
-  console.log('GET / 요청에서만 실행됩니다.');
-  next();
+  let user = req.session.user;
+  if (user) {
+    res.send(`${user.username}님 환영합니다.`);
+  } else {
+    next();
+  }
 }, (req, res) => {
   throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
 });
